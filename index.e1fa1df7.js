97695d4a60c3fc8a7257f6fd5692e1d935b61d80
Davwidos <d.jurkowski015@gmail.com>
2024-02-29 11:28:24 +0100

    make build

---
+++ index.e1fa1df7.js
@@ -0,0 +1,2 @@
+var e,t,c=document.querySelector(".spider"),l=document.querySelector(".wall");e=(l.clientWidth-c.offsetWidth)/2,t=(l.clientHeight-c.offsetHeight)/2,c.style.left="".concat(e,"px"),c.style.top="".concat(t,"px");
+//# sourceMappingURL=index.e1fa1df7.js.map
